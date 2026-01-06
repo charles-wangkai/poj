3fcd267cf8da7a0de56d135211f8f72846be2273
Kai Wang <charles.wangkai@gmail.com>
2026-01-06 20:57:40 +0800

    Katu Puzzle

--- 3678/Main.java
+++ 3678/Main.java
@@ -2,6 +2,7 @@
 // https://cp-algorithms.com/graph/2SAT.html
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
@@ -29,101 +30,140 @@ public class Main {
   }
 
   static boolean solve(int N, int[] a, int[] b, int[] c, String[] op) {
-    @SuppressWarnings("unchecked")
-    List<Integer>[] adjLists = new List[N * 2];
-    for (int i = 0; i < adjLists.length; ++i) {
-      adjLists[i] = new ArrayList<Integer>();
-    }
-
-    @SuppressWarnings("unchecked")
-    List<Integer>[] reversedAdjLists = new List[N * 2];
-    for (int i = 0; i < reversedAdjLists.length; ++i) {
-      reversedAdjLists[i] = new ArrayList<Integer>();
-    }
+    TwoSat twoSat = new TwoSat(N);
 
     for (int i = 0; i < a.length; ++i) {
       if (op[i].equals("AND")) {
         if (c[i] == 1) {
-          addEdge(adjLists, reversedAdjLists, a[i] * 2 + 1, a[i] * 2);
-          addEdge(adjLists, reversedAdjLists, b[i] * 2 + 1, b[i] * 2);
+          twoSat.addClause(a[i], true, a[i], true);
+          twoSat.addClause(b[i], true, b[i], true);
         } else {
-          addEdge(adjLists, reversedAdjLists, a[i] * 2, b[i] * 2 + 1);
-          addEdge(adjLists, reversedAdjLists, b[i] * 2, a[i] * 2 + 1);
+          twoSat.addClause(a[i], false, b[i], false);
         }
       } else if (op[i].equals("OR")) {
         if (c[i] == 1) {
-          addEdge(adjLists, reversedAdjLists, a[i] * 2 + 1, b[i] * 2);
-          addEdge(adjLists, reversedAdjLists, b[i] * 2 + 1, a[i] * 2);
+          twoSat.addClause(a[i], true, b[i], true);
         } else {
-          addEdge(adjLists, reversedAdjLists, a[i] * 2, a[i] * 2 + 1);
-          addEdge(adjLists, reversedAdjLists, b[i] * 2, b[i] * 2 + 1);
+          twoSat.addClause(a[i], false, a[i], false);
+          twoSat.addClause(b[i], false, b[i], false);
         }
       } else if (c[i] == 1) {
-        addEdge(adjLists, reversedAdjLists, a[i] * 2, b[i] * 2 + 1);
-        addEdge(adjLists, reversedAdjLists, b[i] * 2 + 1, a[i] * 2);
-        addEdge(adjLists, reversedAdjLists, a[i] * 2 + 1, b[i] * 2);
-        addEdge(adjLists, reversedAdjLists, b[i] * 2, a[i] * 2 + 1);
+        twoSat.addClause(a[i], false, b[i], false);
+        twoSat.addClause(a[i], true, b[i], true);
       } else {
-        addEdge(adjLists, reversedAdjLists, a[i] * 2, b[i] * 2);
-        addEdge(adjLists, reversedAdjLists, b[i] * 2, a[i] * 2);
-        addEdge(adjLists, reversedAdjLists, a[i] * 2 + 1, b[i] * 2 + 1);
-        addEdge(adjLists, reversedAdjLists, b[i] * 2 + 1, a[i] * 2 + 1);
+        twoSat.addClause(a[i], false, b[i], true);
+        twoSat.addClause(a[i], true, b[i], false);
       }
     }
 
-    List<Integer> sortedNodes = new ArrayList<Integer>();
-    boolean[] visited = new boolean[N * 2];
-    for (int i = 0; i < visited.length; ++i) {
-      if (!visited[i]) {
-        search1(adjLists, sortedNodes, visited, i);
-      }
-    }
-    Collections.reverse(sortedNodes);
+    return twoSat.findAssignment() != null;
+  }
+}
 
-    int k = 0;
-    int[] components = new int[N * 2];
-    for (int node : sortedNodes) {
-      if (components[node] == 0) {
-        ++k;
-        search2(reversedAdjLists, components, node, k);
-      }
-    }
+class TwoSat {
+  int n;
+  Scc scc;
 
-    for (int i = 0; i < N; ++i) {
-      if (components[i * 2] == components[i * 2 + 1]) {
-        return false;
-      }
-    }
-
-    return true;
+  TwoSat(int n) {
+    this.n = n;
+    scc = new Scc(2 * n);
   }
 
-  static void search2(List<Integer>[] reversedAdjLists, int[] components, int node, int k) {
-    components[node] = k;
+  void addClause(int i, boolean f, int j, boolean g) {
+    scc.addEdge(2 * i + (f ? 0 : 1), 2 * j + (g ? 1 : 0));
+    scc.addEdge(2 * j + (g ? 0 : 1), 2 * i + (f ? 1 : 0));
+  }
 
-    for (int adj : reversedAdjLists[node]) {
-      if (components[adj] == 0) {
-        search2(reversedAdjLists, components, adj, k);
+  boolean[] findAssignment() {
+    int[] components = scc.buildComponents();
+
+    boolean[] assignment = new boolean[n];
+    for (int i = 0; i < n; ++i) {
+      if (components[2 * i] == components[2 * i + 1]) {
+        return null;
       }
+
+      assignment[i] = components[2 * i] < components[2 * i + 1];
+    }
+
+    return assignment;
+  }
+}
+
+class Scc {
+  List<Integer>[] adjLists;
+  List<Integer>[] reversedAdjLists;
+
+  @SuppressWarnings("unchecked")
+  Scc(int n) {
+    adjLists = new List[n];
+    for (int i = 0; i < adjLists.length; ++i) {
+      adjLists[i] = new ArrayList<Integer>();
+    }
+
+    reversedAdjLists = new List[n];
+    for (int i = 0; i < reversedAdjLists.length; ++i) {
+      reversedAdjLists[i] = new ArrayList<Integer>();
     }
   }
 
-  static void addEdge(
-      List<Integer>[] adjLists, List<Integer>[] reversedAdjLists, int from, int to) {
+  void addEdge(int from, int to) {
     adjLists[from].add(to);
     reversedAdjLists[to].add(from);
   }
 
-  static void search1(
-      List<Integer>[] adjLists, List<Integer> sortedNodes, boolean[] visited, int node) {
+  List<Integer> topologicalSort() {
+    int n = adjLists.length;
+
+    List<Integer> sorted = new ArrayList<Integer>();
+    boolean[] visited = new boolean[n];
+    for (int i = 0; i < n; ++i) {
+      if (!visited[i]) {
+        search1(sorted, visited, i);
+      }
+    }
+    Collections.reverse(sorted);
+
+    return sorted;
+  }
+
+  private void search1(List<Integer> sorted, boolean[] visited, int node) {
     visited[node] = true;
 
     for (int adj : adjLists[node]) {
       if (!visited[adj]) {
-        search1(adjLists, sortedNodes, visited, adj);
+        search1(sorted, visited, adj);
       }
     }
 
-    sortedNodes.add(node);
+    sorted.add(node);
+  }
+
+  int[] buildComponents() {
+    int n = adjLists.length;
+
+    List<Integer> sorted = topologicalSort();
+
+    int[] components = new int[n];
+    Arrays.fill(components, -1);
+    int component = 0;
+    for (int node : sorted) {
+      if (components[node] == -1) {
+        search2(components, node, component);
+        ++component;
+      }
+    }
+
+    return components;
+  }
+
+  private void search2(int[] components, int node, int component) {
+    components[node] = component;
+
+    for (int adj : reversedAdjLists[node]) {
+      if (components[adj] == -1) {
+        search2(components, adj, component);
+      }
+    }
   }
 }
